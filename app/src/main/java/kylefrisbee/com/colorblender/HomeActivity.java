bfc5c2273b2aa14d2e02e59d2a715ccd03289649
Ryan <rnewsom@msudenver.edu>
2015-09-19 17:42:17 -0600

    Should be working

--- app/src/main/java/kylefrisbee/com/colorblender/HomeActivity.java
+++ app/src/main/java/kylefrisbee/com/colorblender/HomeActivity.java
@@ -4,12 +4,14 @@ import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,6 +19,11 @@ public class HomeActivity extends AppCompatActivity {
     private Button mColor1;
     private Button mColor2;
     private SeekBar mSlider;
+    private int mRedValue;
+    private int mGreenValue;
+    private int mBlueValue;
+    private Bundle mBundle;
+    private RelativeLayout mSquareColor;
 
 
     @Override
@@ -27,20 +34,51 @@ public class HomeActivity extends AppCompatActivity {
         mColor1 = (Button) findViewById(R.id.color1_button);
         mColor2 = (Button) findViewById(R.id.color2_button);
         mSlider = (SeekBar) findViewById(R.id.seekBar);
+        mSquareColor = (RelativeLayout) findViewById(R.id.MainLayout);
 
         addListeners();
+        mBundle = savedInstanceState;
     }
 
     private void addListeners() {
         mColor1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_RUN);
-                i.setComponent(new ComponentName("com.kylefrisbie.colorpicker.app", "MainActivity.java"));
-                i.putExtra("Widget", "Button1");
-                startActivityForResult(i,RESULT_OK);
+                getColor(SettingsFragment.COLOR_1_BUTTON);
             }
         });
+
+        mColor2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getColor(SettingsFragment.COLOR_2_BUTTON);
+            }
+        });
+
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                blendColors(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void getColor(String component) {
+        mBundle.putString("COMPONENT", component);
+        Intent i = new Intent(Intent.ACTION_RUN);
+        i.setComponent(new ComponentName("com.kylefrisbie.colorpicker.app", "MainActivity.java"));
+        startActivityForResult(i, RESULT_OK);
+
     }
 
     @Override
@@ -67,4 +105,48 @@ public class HomeActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_OK) {
+            if (data != null) {
+                mRedValue = data.getIntExtra("R", -1);
+                mGreenValue = data.getIntExtra("G", -1);
+                mBlueValue = data.getIntExtra("B", -1);
+                changeColor(mBundle.getString("COMPONENT", ""));
+            }
+        }
+    }
+
+    /**
+     * Changed the color of the component the user requested based off the color they chose with the color picker
+     * @param component - the widget to change
+     */
+    private void changeColor(String component) {
+        switch(component){
+            case SettingsFragment.COLOR_1_BUTTON:
+                mColor1.setBackgroundColor(Color.rgb(mRedValue, mGreenValue, mBlueValue));
+                break;
+            case SettingsFragment.COLOR_2_BUTTON:
+                mColor2.setBackgroundColor(Color.rgb(mRedValue, mGreenValue, mBlueValue));
+                break;
+            case SettingsFragment.SLIDER:
+                mSlider.setBackgroundColor(Color.rgb(mRedValue, mGreenValue, mBlueValue));
+                break;
+
+        }
+    }
+
+    private int blendColors(float ratio) {
+        int color1 = mColor1.getSolidColor();
+        int color2 = mColor2.getSolidColor();
+        final float inverseRation = 1f - ratio;
+        float r = (Color.red(color1) * ratio) + (Color.red(color2) * inverseRation);
+        float g = (Color.green(color1) * ratio) + (Color.green(color2) * inverseRation);
+        float b = (Color.blue(color1) * ratio) + (Color.blue(color2) * inverseRation);
+        return Color.rgb((int) r, (int) g, (int) b);
+    }
+
 }
--- app/src/main/java/kylefrisbee/com/colorblender/SettingsFragment.java
+++ app/src/main/java/kylefrisbee/com/colorblender/SettingsFragment.java
@@ -1,6 +1,9 @@
 package kylefrisbee.com.colorblender;
 
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +17,11 @@ public class SettingsFragment extends Fragment {
     private Button mButton1;
     private Button mButton2;
     private Button mSliderButton;
+    public static final String COLOR_1_BUTTON = "COLOR_1";
+    public static final String COLOR_2_BUTTON = "COLOR_2";
+    public static final String SLIDER = "SLIDER";
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,29 +47,30 @@ public class SettingsFragment extends Fragment {
         mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getColorFromPicker();
+                getColorFromPicker(COLOR_1_BUTTON);
             }
         });
 
         mButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getColorFromPicker();
+                getColorFromPicker(COLOR_2_BUTTON);
             }
         });
 
         mSliderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getColorFromPicker();
+                getColorFromPicker(SLIDER);
             }
         });
     }
 
-    private void getColorFromPicker() {
-        //getArguments().putBoolean("", true );
-        //remove this fragment
-        //go to color picker app and get color.
-        //once color is determined, set it to one of the views
+    private void getColorFromPicker(String component) {
+        getArguments().putString("COMPONENT",component);
+        getFragmentManager().popBackStack();
+        Intent i = new Intent(Intent.ACTION_RUN);
+        i.setComponent(new ComponentName("com.kylefrisbie.colorpicker.app", "MainActivity.java"));
+        getActivity().startActivityForResult(i, Activity.RESULT_OK);
     }
 }
